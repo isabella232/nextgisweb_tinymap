530f0a7be32e7781db8acd55f8e5b9417ab516f4
trolleway <trolleway@yandex.ru>
2016-03-18 16:07:21 +0300

    demo url

--- tinymap.js
+++ tinymap.js
@@ -21,7 +21,7 @@ var nRequest = new Array;
 
 
 //var ngwLayerURL='http://78.46.100.76/opendata_ngw/api/resource/586'    //production
-var ngwLayerURL='http://176.9.38.120/practice2/api/resource/29'     //debug
+var ngwLayerURL='http://176.9.38.120/practice2/api/resource/31'     //debug
 
 
 
