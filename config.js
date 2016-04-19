2484c0a6bfaf657f499afc550e8d824c2308a5b5
trolleway <trolleway@yandex.ru>
2016-04-19 14:01:21 +0300

    update demo

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 
 //http://176.9.38.120/practice2/api/resource/31
 var config = {
-  NGWLayerURL: 'http://176.9.38.120/practice2/api/resource/31', 
+  NGWLayerURL: 'http://78.46.100.76/opendata_ngw/api/resource/594', 
 
 NGWPhotoThumbnailSize : '400x300',
 NGWLayerAttribution:'<a href="http://primorsky.ru/">Исходные данные: Официальный сайт Администрации Приморского края</a>',
+   DefaultBBOXMode:'manual',        //Optional
+    lat:43.8,                       //Optional
+    lon:134.6,                      //Optional
+    zoom:8,                         //Optional
 
 }
