53d8a069bae38e428a6e5ce08ac89fcca560bbfc
Artem Svetlov <trolleway@yandex.ru>
2016-05-13 19:38:03 +0300

    url change

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 //http://176.9.38.120/practice2/api/resource/31
 var config = {
-  NGWLayerURL: 'http://78.46.100.76/opendata_ngw/api/resource/594', 
+  NGWLayerURL: 'http://176.9.38.120/practice2/api/resource/322',
 
 NGWPhotoThumbnailSize : '400x300',
 NGWLayerAttribution:'<a href="http://primorsky.ru/">Исходные данные: Официальный сайт Администрации Приморского края</a>',
